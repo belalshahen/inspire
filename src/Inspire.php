bef8695dc3f281ab2ad219c1aecadc5172436ecd
belalshahen <106923197+belalshahen@users.noreply.github.com>
2022-11-18 14:29:16 +0300

    up

--- src/Inspire.php
+++ src/Inspire.php
@@ -6,8 +6,9 @@ use Illuminate\Support\Facades\Http;
 
 class Inspire {
     public function justDoIt() {
-            $response = Http::get('https://inspiration.goprogram.ai/');
+         //   $response = Http::get('https://inspiration.goprogram.ai/');
 
-        return $response['quote'] . ' -' . $response['author'];
+       // return $response['quote'] . ' -' . $response['author'];
+	   return 1;
     }
 }
